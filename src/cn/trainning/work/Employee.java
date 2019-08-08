956e6e05fc063a981156b60e62406648cab7c176
Superkooli <1421213830@qq.com>
2019-08-08 22:48:20 +0800

    ss

--- src/cn/trainning/work/Employee.java
+++ src/cn/trainning/work/Employee.java
@@ -1,17 +1,76 @@
-package cn.trainning.work;
+package cn.tranning.work;
 
+import java.util.Date;
 
 public class Employee {
-	private int 
-	EMPLOYEE_ID
-	FIRST_NAME
-	LAST_NAME
-	EMAIL
-	PHONE_NUMBER
-	HIRE_DATE
-	JOB_ID
-	SALARY
-	COMMISSION_PCT
-	MANAGER_ID
-	DEPARTMENT_ID
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private Date hireDate;
+	private String jobId;
+	private double salary;
+	private int managerId;
+	private int deptId;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Date getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public int getManagerId() {
+		return managerId;
+	}
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
 }
--- src/cn/trainning/work/QueryData.java
+++ src/cn/trainning/work/QueryData.java
@@ -1,22 +1,22 @@
 package cn.trainning.work;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class QueryData {
 
-	private static String driver = "oracle.jdbc.driver.OracleDriver"; //驱动
+	private static String driver = "oracle.jdbc.driver.OracleDriver"; // 驱动
 
-    private static String url = "jdbc:oracle:thin:@//127.0.0.1:1521/orcl"; //连接字符串
+	private static String url = "jdbc:oracle:thin:@//127.0.0.1:1521/orcl"; // 连接字符串
 
-    private static String username = "scott"; //用户名
+	private static String username = "scott"; // 用户名
 
-    private static String password = "tiger"; //密码
+	private static String password = "tiger"; // 密码
 
 	public static Connection getConnection() {
 		Connection conn = null;
@@ -29,20 +29,51 @@ public class QueryData {
 		}
 		return conn;
 	}
-	
+
 	public static List<? extends Object> getData(String type, String sql) {
-		Connection  conn = getConnection();
+		Connection conn = getConnection();
 		Statement stat = null;
 		ResultSet rs = null;
+		List<Employee> empResult = new ArrayList<>();
+		List<Department> deptResult = new ArrayList<>();
 		try {
 			stat = conn.createStatement();
 			rs = stat.executeQuery(sql);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		while
+		while (rs.next()) {
+
+			if ("emp".equals(type)) {
+				Employee emp = new Employee();
+				emp.setEmpId(rs.getString("EMPLOYEE_ID"));
+				EMAIL
+				PHONE_NUMBER
+				HIRE_DATE
+				JOB_ID
+				SALARY
+				COMMISSION_PCT
+				MANAGER_ID
+				DEPARTMENT_ID
+				emp.setFirstName(rs.getString("FIRST_NAME"));
+				emp.setLastName(rs.getString("LAST_NAME"));
+				emp.setEmail(rs.getString("EMAIL"));
+				emp.setHireDate(rs.getDate(columnIndex));
+				emp.setJobId(rs.getInt(columnIndex));
+				emp.setManagerId(managerId);
+				emp.setDeptId(rs.getInt(columnIndex));
+				emp.setSalary(salary);
+				emp.setPhone(phone);
+				return empResult;
+			}
+			if ("dept".equals(type)) {
+
+				return deptResult;
+			}
+		}
+		return null;
 	}
-	
+
 	public static void closeConnection(Connection conn, Statement stat, ResultSet rs) {
 		try {
 			conn.close();
